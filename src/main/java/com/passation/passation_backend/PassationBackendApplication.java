c602bb433fa009161547a9f5d54aa1370fc6561a
Driss <lahbilidriss@gmail.com>
2026-05-05 20:45:00 +0000

    chore: initial project setup - Spring Boot backend

---
+++ src/main/java/com/passation/passation_backend/PassationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.passation.passation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passation/passation_backend/PassationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passation.passation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

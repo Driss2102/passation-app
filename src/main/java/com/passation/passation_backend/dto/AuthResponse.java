f406e12f789e38b0750b926aa96ce4b47baedf4f
Driss <lahbilidriss@gmail.com>
2026-05-06 17:01:42 +0000

    feat: add JWT utility, filter, CustomUserDetailsService and Auth endpoints (SCRUM-17, SCRUM-18)

---
+++ src/main/java/com/passation/passation_backend/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.passation.passation_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String role;
+    private String nom;
+    private String prenom;
+}
---
+++ src/main/java/com/passation/passation_backend/model/Role.java
@@ -0,0 +1,8 @@
+package com.passation.passation_backend.model;
+
+public enum Role {
+    EMPLOYE,
+    MANAGER_RH,
+    REMPLACANT,
+    ADMIN
+}
